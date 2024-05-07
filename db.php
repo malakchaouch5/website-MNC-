76f4b84a3ba236993daf331def66024ccde317a5
nourchenerm <155021665+nourchenerm@users.noreply.github.com>
2024-05-07 13:19:34 +0100

    db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php 
+// Connexion à la base de données
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ecommerce";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "La connexion a échoué : " . $e->getMessage();
+}
+?>
